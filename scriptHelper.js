461e55cc960103521f501ec42e1201f0723fd35a
allybreeden <allybreeden@gmail.com>
2023-03-08 20:16:14 -0600

    for testing purposes

--- scriptHelper.js
+++ scriptHelper.js
@@ -84,11 +84,16 @@ async function myFetch() {
     let planetsReturned;
 
     planetsReturned = await fetch('https://handlers.education.launchcode.org/static/planets.json').then( function(response) {
-        return response.json();
-        });
+       if (response.status >= 400) {
+            throw new Error ("Error response");
+        } else {
+            return response.json();
+        }
+     });
 
     return planetsReturned;
 }
+
 
 function pickPlanet(planets) {
     let num = Math.floor(Math.random() * planets.length);
